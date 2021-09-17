552edd00beb21e3e6f7d54a58be21a4c32b5a889
Prasanna Gurung <pgurung1@my.hpu.edu>
2021-09-17 23:06:00 +0000

    Implemented user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_193225) do
+ActiveRecord::Schema.define(version: 2021_09_17_223655) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string "content"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 2021_09_14_193225) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
   add_foreign_key "todo_items", "todo_lists"

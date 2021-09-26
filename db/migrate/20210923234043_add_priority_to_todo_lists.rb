18ad945977c6cf6db482f7bf1ddf6a6e51b2612a
Prasanna Gurung <pgurung1@my.hpu.edu>
2021-09-26 23:40:49 +0000

    updated priority field

--- db/migrate/20210923234043_add_priority_to_todo_lists.rb
+++ db/migrate/20210923234043_add_priority_to_todo_lists.rb
@@ -1,6 +1,6 @@
 class AddPriorityToTodoLists < ActiveRecord::Migration[6.1]
   def change
-    add_column :todo_lists, :priority, :string, null:false
+    add_column :todo_lists, :priority, :string
   end
 #  drop_table :todo_items
 end

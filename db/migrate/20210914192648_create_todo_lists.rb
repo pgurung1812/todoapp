65268b93a32738a7adc038dae04e844790cd7816
Ubuntu <ubuntu@ip-172-31-30-153.us-east-2.compute.internal>
2021-10-05 23:05:51 +0000

    Edit migration file so that Heroku app works again

--- db/migrate/20210914192648_create_todo_lists.rb
+++ db/migrate/20210914192648_create_todo_lists.rb
@@ -3,10 +3,10 @@ class CreateTodoLists < ActiveRecord::Migration[6.1]
     create_table :todo_lists do |t|
       t.string :title, null: false
       t.text :description, null: false
-      t.belongs_to :user,null: false, foreign_key:true
+      # t.belongs_to :user,null: false, foreign_key:true
 
       t.timestamps null: false
     end
-    add_index:todo_lists, [:user_id, :title]
+    # add_index:todo_lists, [:user_id, :title]
   end
 end

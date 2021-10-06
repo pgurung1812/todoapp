93255faf5ffcb250404b09872f6e214e168b768b
Prasanna Gurung <pgurung1@my.hpu.edu>
2021-10-06 01:53:32 +0000

    updated file to fix migration

--- db/migrate/20210917223655_create_users.rb
+++ db/migrate/20210917223655_create_users.rb
@@ -7,6 +7,17 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :confirmation_token, limit: 128
       t.string :remember_token, limit: 128, null: false
     end
+    
+    drop_table :todo_lists, force: :cascade
+    
+    create_table :todo_lists do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.belongs_to :user,null: false, foreign_key:true
+
+      t.timestamps null: false
+    end
+    add_index:todo_lists, [:user_id, :title]
 
     add_index :users, :email
     add_index :users, :remember_token

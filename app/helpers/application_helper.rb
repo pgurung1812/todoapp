8e5cfe68412a07f687fba48f49298d0c63d6e313
Prasanna Gurung <pgurung1@my.hpu.edu>
2021-10-11 07:38:01 +0000

    added sorting feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def sort_link(column, title = nil)
+    title ||= column.titleize
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    icon = sort_direction == "asc" ? "bi bi-caret-down" : "bi bi-caret-up"
+    icon = column == sort_column ? icon : ""
+    link_to "#{title} <span class='#{icon}'></span>".html_safe, {column: column, direction: direction}
+  end
 end

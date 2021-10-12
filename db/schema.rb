c05ae4a3731a5bfc96c23b5ebe5eeb56a94ff216
Ubuntu <ubuntu@ip-172-31-30-153.us-east-2.compute.internal>
2021-10-12 04:23:51 +0000

    Run db:migration locally

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@ ActiveRecord::Schema.define(version: 2021_09_23_234043) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string "content"
+    t.integer "todo_list_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
   create_table "todo_lists", force: :cascade do |t|
@@ -44,5 +46,6 @@ ActiveRecord::Schema.define(version: 2021_09_23_234043) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "todo_items", "todo_lists"
   add_foreign_key "todo_lists", "users"
 end
